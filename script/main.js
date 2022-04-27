a0ec6b6f7698250e5825b8061d48b8c7d98be5cf
Radya Mulia <radyamulia@gmail.com>
2022-04-27 21:03:12 +0800

    second commit

--- script/main.js
+++ script/main.js
@@ -1,15 +1,19 @@
+const arrayData = {
+    prodDataInput: function(name, price, pic) {
+        this.name = name;
+        this.price = price;
+        this.picture = pic;
+    }
+}
+
 function inputData() {
     const prodName = document.getElementById('prodName').value
     const prodPrice = document.getElementById('prodPrice').value
     const prodPic = document.getElementById('prodPic').value
 
-    const arrayData = new Object();
-
-    arrayData.prodData = (name, price, pic) => {
-        this['name'] = name;
-        this['price'] = price;
-        this['picture'] = pic;
-    }
-
+    const prodData = new arrayData();
+    
+    const stringifiedArrayObj = JSON.stringify(prodData.prodDataInput(prodName, prodPrice, prodPic))
+    localStorage.setItem
     
 }
